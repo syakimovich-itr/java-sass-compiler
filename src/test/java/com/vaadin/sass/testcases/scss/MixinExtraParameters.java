13825af24365e7643eab4b43b35bae4d5426e6ec
volker <volker@vb6.internal.inetsoftware.de>
2023-05-12 10:20:15 +0200

    test angepasst

--- src/test/java/com/vaadin/sass/testcases/scss/MixinExtraParameters.java
+++ src/test/java/com/vaadin/sass/testcases/scss/MixinExtraParameters.java
@@ -10,17 +10,17 @@ public class MixinExtraParameters extends AbstractTestBase {
     String scss = "/scss/mixin-extra-params.scss";
 
     @Test
-    public void testCompiler() {
+    public void testCompiler() throws Exception {
         ScssStylesheet sheet;
         try {
             sheet = getStyleSheet(scss);
             sheet.compile();
-        } catch (RuntimeException e) {
-            Assert.assertEquals(
-                    "Substitution error: some actual parameters were not used. Formal parameters: FormalArgumentList[$p1: null], actual parameters: Actual argument list [ArgumentList [foo, bar]] at line 4, column 14",
-                    e.getMessage());
-        } catch (Exception e) {
             Assert.fail();
+        } catch (AssertionError e) {
+            Throwable th = e.getCause();
+            Assert.assertEquals(
+                    "Substitution error: some actual parameters were not used. Formal parameters: FormalArgumentList[$p1: null], actual parameters: Actual argument list [ArgumentList [foo, bar]] at line 4, column 15, in file mixin-extra-params.scss",
+                    th.getMessage());
         }
     }
 }
